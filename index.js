30788bc8abe94b45c403f7f912ce38913c27215d
Prashant2468 <prashantpatil932870@gmail.com>
2024-11-17 02:03:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors"); // Import cors
+const app = express();
+const books = require("./Book.json");
+const PORT = 5000;
+
+app.use(cors()); // Enable CORS
+
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+// app.get("/books/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const book = books.find((book) => book.id === id);
+//   res.json(book);
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
